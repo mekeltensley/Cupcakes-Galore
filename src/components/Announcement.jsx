aaf98c2690ddc60a0497e4768dffd1ea607f7e9a
Mekel Tensley <mekelintech@gmail.com>
2022-01-03 15:20:22 -0500

    Create an announcement bar

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 55px;
+    background-color: #EFBDEB;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px;
+    text-align: center;
+    font-weight: 500;
+`;
+
+
+class Announcement extends Component {
+    render() {
+        return (
+            <div>
+                <Container>
+                    Grand Cyber Opening! Buy Two or More Cupcakes, Get One Free
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Announcement;
