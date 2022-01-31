212bb774ea36cb341f610dc35d3aba92f0e0c04e
Mekel Tensley <mekelintech@gmail.com>
2022-01-31 17:23:39 -0500

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,38 @@
-import Home from "./components/Home";
 import React, { Component } from "react";
-import ProductList from "./components/ProductList";
+import Home from "./components/Home";
+import Classic from "./components/Classic";
+import Specialty from "./components/Specialty";
 import Product from "./components/Product";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Cart from "./components/Cart";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from 'react-router-dom';
+
+
 
 class App extends Component {
   render() {
     return (
-      <Cart />
-
+      <Router>
+      <div>
+       <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/classic" element={<Classic />}/>
+        <Route path="/specialty" element={<Specialty />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/login" element={<Login />}/>
+        </Routes>
+      </div>
+    </Router>
     );
   }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
